90b88369269834dc2b74ae9991c10fe2f97aab63
Asif Ahamed <asifshezan7@gmail.com>
2022-08-13 11:43:00 +0600

    how to use style sheet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const todoTitle = "She is my wife.";
@@ -7,11 +8,26 @@ const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const yearFull = date.getFullYear();
+
+const headingStyle = {
+    background: "purple",
+    color: "white",
+    textAlign: "center",
+    padding: "10px"
+}
+
 root.render(
     <div>
-    <h2>{ todoTitle }</h2>
+        {/* External Css */}
+    <h1 style={headingStyle}>React App</h1>
+
+        {/* Inline Css */}
+    <h2 style={{ color:"blue", fontSize:"20px", fontFamily: "sans-serif", textTransform:"uppercase"}}>{ todoTitle }</h2> 
+    
+        {/* Style-Sheet  style.css file*/}
+    <h1 className='heading largetext'>Welcome Asif</h1>
+
     <p>lorem ipsum is a dubbing text website.</p>
-    <h1>Welcome Asif</h1>
     <h2>To make aa good app.</h2>
     <h3>{ dateName + "/" + monthName + "/" + yearFull }</h3>
     </div>

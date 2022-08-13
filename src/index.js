393a5c0a34cd7cc06c6b52ea9b22e5a0d38afe5c
Asif Ahamed <asifshezan7@gmail.com>
2022-08-13 18:32:15 +0600

    design a card

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 // import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const todoTitle = "She is my wife.";
+
+
+const todoTitle = "React App";
+const todoDesc = "Lorem ipsum is a dubbing text website.";
+
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
@@ -19,17 +23,31 @@ const headingStyle = {
 root.render(
     <div>
         {/* External Css */}
-    <h1 style={headingStyle}>React App</h1>
+    <h1 style={ headingStyle }>React App</h1>
 
         {/* Inline Css */}
-    <h2 style={{ color:"blue", fontSize:"20px", fontFamily: "sans-serif", textTransform:"uppercase"}}>{ todoTitle }</h2> 
+    {/* <h2 style={{ color:"blue", fontSize:"20px", fontFamily: "sans-serif", textTransform:"uppercase"}}>{ todoTitle }</h2>  */}
     
         {/* Style-Sheet  style.css file*/}
     <h1 className='heading largetext'>Welcome Asif</h1>
 
-    <p>lorem ipsum is a dubbing text website.</p>
-    <h2>To make aa good app.</h2>
-    <h3>{ dateName + "/" + monthName + "/" + yearFull }</h3>
+    <div className='card'>
+        <h2 className='cardTitle'>{ todoTitle }</h2>
+        <p className='cardDesc'>{ todoDesc }</p>
+        <h3 className='cardFooter'>{ dateName + "/" + monthName + "/" + yearFull }</h3>
     </div>
+
+    <div className='card'>
+        <h2 className='cardTitle'>{ todoTitle }</h2>
+        <p className='cardDesc'>{ todoDesc }</p>
+        <h3 className='cardFooter'>{ dateName + "/" + monthName + "/" + yearFull }</h3>
+    </div>
+
+    <div className='card'>
+        <h2 className='cardTitle'>{ todoTitle }</h2>
+        <p className='cardDesc'>{ todoDesc }</p>
+        <h3 className='cardFooter'>{ dateName + "/" + monthName + "/" + yearFull }</h3>
+    </div>
+</div>
 
 );

329af9fdb5d371282c0f7f3e8e255bcba13c5ebc
Asif Ahamed <asifshezan7@gmail.com>
2022-08-14 20:31:06 +0600

    Array and Object Destructuring

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,21 @@ const dateName = date.getDate();
 const monthName = date.getMonth();
 const yearFull = date.getFullYear();
 
+// Array Destructuring
+const name = ["dog", "cat"];
+name[0];
+name[1];
+
+const ["dog","cat"] = name;
+const dog = name[0];
+const cat = name[1];
+
 function Card(props){
+    // object Destructuring
+    const {titleText, descText} = props
     return <div className='card'>
-    <h2 className='cardTitle'>{ props.titleText }</h2>
-    <p className='cardDesc'>{ props.descText }</p>
+    <h2 className='cardTitle'>{ titleText }</h2>
+    <p className='cardDesc'>{ descText }</p>
     <h3 className='cardFooter'>{ dateName + "/" + monthName + "/" + yearFull }</h3>
 </div>
 }

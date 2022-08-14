ca2dcf0802f006ba075e28a528ed35d297cea026
Asif Ahamed <asifshezan7@gmail.com>
2022-08-14 21:57:11 +0600

    Data pass by using For Loop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import Card from "./components/Card";
 import Data from "./data.json";
 
 function App(){
+
+    let items = [];
+    for (let x = 0; x < Data.length; x++) {
+        items.push( <Card titleText= { Data[x].title } descText= { Data[x].desc }/> );
+    }
+
+
     return <div>
         <h1 className='headingStyle'>React App</h1>
-        <Card titleText= { Data[0].title } descText= { Data[0].desc }/>
-        <Card titleText= { Data[1].title } descText= { Data[1].desc }/>
+        {items}
     </div>
 }
 

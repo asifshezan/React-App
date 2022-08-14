ee40c83200e5b3275cedf4f09d073b9f423b88ef
Asif Ahamed <asifshezan7@gmail.com>
2022-08-14 20:09:14 +0600

    props using

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Card from "./components/Card";
 function App(){
     return <div>
         <h1 className='headingStyle'>React App</h1>
-        <Card />
+        <Card titleText="Call Mother" descText="His mother is so roude."/>
+        <Card titleText="Call Mother" descText="His mother is so roude."/>
+        <Card titleText="Call Mother" descText="His mother is so roude."/>
     </div>
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-const todoTitle = "React App";
-const todoDesc = "Lorem ipsum is a dubbing text website.";
+// const todoTitle = "React App";
+// const todoDesc = "Lorem ipsum is a dubbing text website.";
 
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const yearFull = date.getFullYear();
 
-function Card(){
+function Card(props){
     return <div className='card'>
-    <h2 className='cardTitle'>{ todoTitle }</h2>
-    <p className='cardDesc'>{ todoDesc }</p>
+    <h2 className='cardTitle'>{ props.titleText }</h2>
+    <p className='cardDesc'>{ props.descText }</p>
     <h3 className='cardFooter'>{ dateName + "/" + monthName + "/" + yearFull }</h3>
 </div>
 }

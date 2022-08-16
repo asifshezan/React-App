f6413086455ee1394cc1c80ef3452c586eabd6a4
Asif Ahamed <asifshezan7@gmail.com>
2022-08-16 20:30:25 +0600

    nested mapping

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-import List from "./components/UniqueList/List";
-import Card from "./components/Card";
-import Data from "./data.json";
+// import List from "./components/UniqueList/List";
+// import Card from "./components/Card";
+// import Data from "./data.json";
 
+const users = [
+    {
+        fullName : "Asif Ahamed",
+        email : "asifshezan7@gmail.com",
+        phones : [
+            {home : "01121545"}, {office : "01521131545"}
+        ]
+    },
+    {
+        fullName : "Sonia Akter",
+        email : "soniafridi98@gmail.com",
+        phones : [
+            {home : "016554513"}, {office : "011265461"}
+        ]
+    }
+];
 
 
 function App(){
@@ -16,15 +32,36 @@ function App(){
     // }
 
 
-    return <div>
+    return (<div>
         <h1 className='headingStyle'>React App</h1>
 
-        { Data.map((item, index) => <Card key={index} titleText = {item.title} descText = {item.desc} /> ) }
+        {
+            users.map((user, index) =>
+            <article>
+                <h2>{ user.fullName }</h2>
+                <h3>{ user.email }</h3>
+                {
+                    user.phones.map((phone, index)=>
+                    
+                    <div key={index}>
+                        <h4>{phone.home}</h4>
+                        <h4>{phone.office}</h4>
+                    </div>
+                    )
+                }
+            </article>
+            )
+        }
 
+
+
+
+
+        {/* { Data.map((item, index) => <Card key={index} titleText = {item.title} descText = {item.desc} /> ) }
         <List />
-
-        {/* {items} */}
+        {items} */}
     </div>
+    );
 }
 
 

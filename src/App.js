5716a550b4ad3967076f96fcdcb13599adf12349
Asif Ahamed <asifshezan7@gmail.com>
2022-08-16 20:47:37 +0600

    data pass by functional component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React from "react";
 // import List from "./components/UniqueList/List";
 // import Card from "./components/Card";
 // import Data from "./data.json";
+import Card2 from "./components/Card2";
 
-const users = [
-    {
-        fullName : "Asif Ahamed",
-        email : "asifshezan7@gmail.com",
-        phones : [
-            {home : "01121545"}, {office : "01521131545"}
-        ]
-    },
-    {
-        fullName : "Sonia Akter",
-        email : "soniafridi98@gmail.com",
-        phones : [
-            {home : "016554513"}, {office : "011265461"}
-        ]
-    }
-];
+// const users = [
+//     {
+//         fullName : "Asif Ahamed",
+//         email : "asifshezan7@gmail.com",
+//         phones : [
+//             {home : "01121545"}, {office : "01521131545"}
+//         ]
+//     },
+//     {
+//         fullName : "Sonia Akter",
+//         email : "soniafridi98@gmail.com",
+//         phones : [
+//             {home : "016554513"}, {office : "011265461"}
+//         ]
+//     }
+// ];
 
 
 function App(){
@@ -35,7 +36,12 @@ function App(){
     return (<div>
         <h1 className='headingStyle'>React App</h1>
 
-        {
+        <Card2 name="Asif Ahamed" age="23"/>
+
+
+
+
+        {/* {
             users.map((user, index) =>
             <article>
                 <h2>{ user.fullName }</h2>
@@ -51,10 +57,7 @@ function App(){
                 }
             </article>
             )
-        }
-
-
-
+        } */}
 
 
         {/* { Data.map((item, index) => <Card key={index} titleText = {item.title} descText = {item.desc} /> ) }
